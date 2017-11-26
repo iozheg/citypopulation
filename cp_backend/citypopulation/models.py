01173055a0f478ef044ef01ef099bbf4bdb10849
alex <mozg@eml.ru>
2017-11-27 00:35:22 +0500

    Comments added.

--- cp_backend/citypopulation/models.py
+++ cp_backend/citypopulation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class CityModel(models.Model):
+    """ City model. """
 
     name = models.CharField(max_length=50)
     population = models.IntegerField()
--- cp_backend/citypopulation/views.py
+++ cp_backend/citypopulation/views.py
@@ -1,8 +1,14 @@
+""" Handling request from app. 
+
+CSRF check is skipped (@csrf_exempt) for simplicity.
+For user management used Django auth system.
+"""
+
 import hashlib
 import json
 
 from django.shortcuts import render
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -12,17 +18,19 @@ from .models import CityModel
 
 @csrf_exempt
 def login_user(request):
+    """ Logins user."""
+
     if request.method == 'POST':
         try:
             username = request.POST.get('username')
             password = request.POST.get('password')
-        except Exception:
+        except KeyError:
             return HttpResponse("username or password is not specified!")
 
     try:
         user = User.objects.get(username=username)
-        hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        user = authenticate(username=username, password=hash)
+        password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        user = authenticate(username=username, password=password_hash)
         if user is not None:
             login(request, user)
             return HttpResponse("user logged in")
@@ -34,30 +42,41 @@ def login_user(request):
 @csrf_exempt
 @login_required(login_url='/')
 def logout_user(request):
+    """ Logouts user. """
+
     logout(request)
     return HttpResponseRedirect('/')
 
 
 @csrf_exempt
 def register(request):
+    """Registers new user."""
+
     if request.method == 'POST':
         try:
             username = request.POST.get('username')
             password = request.POST.get('password')
-        except Exception:
+        except KeyError:
             return HttpResponse("username or password is not specified!")
 
     try:
         user = User.objects.get(username=username)
         return HttpResponse("such user exists")
     except User.DoesNotExist:
-        hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        user = User.objects.create_user(username=username, password=hash)
+        password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        user = User.objects.create_user(
+                        username=username,
+                        password=password_hash
+                    )
         login(request, user)
         return HttpResponse("user logged in")
 
 @login_required(login_url='/')
 def get_cities(request):
+    """ Returns list of cities.
+
+    User must be logged in.
+    """
     if request.method == 'GET':
         cities = CityModel.objects.all()
         return HttpResponse(
