cc1a970f8df7a568dffd6e41753636248510141d
alex <mozg@eml.ru>
2017-11-26 22:47:48 +0500

    Added button 'logoff'.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { Login } from "./Login";
 import { CityManager } from "./CityManager";
+import { serverRequest } from "../utils/http-request-helper";
 
 export interface AppState{
     isLogged: boolean
@@ -18,14 +19,26 @@ export class App extends React.Component<{}, AppState>{
     handleLoginSuccess(){
         this.setState({isLogged: true});
     }
+    
+    logoff(){
+        serverRequest('logoff', {});
+        this.setState({isLogged: false});
+    }
 
     render(){
+        if(this.state.isLogged){
+            return(
+                <div className="container">
+                    <button className="btn" onClick={e => this.logoff()}>
+                        Log off
+                    </button>            
+                    <CityManager />
+                </div>
+            );
+        }
         return(
             <div className="container">
-                {this.state.isLogged
-                    ? <CityManager />
-                    : <Login onLogin={this.handleLoginSuccess} />
-                }
+                <Login onLogin={this.handleLoginSuccess} />
             </div>
         );
     }
